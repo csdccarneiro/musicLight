5433c64c05d361b5c7337190ad0d61be0baf17aa
Caique dos Santos <csdccarneiro@gmail.com>
2022-04-21 11:39:03 -0300

    Removendo tela de configurações da navegação

--- src/navigation/App/BottomTab/index.js
+++ src/navigation/App/BottomTab/index.js
@@ -6,7 +6,6 @@ import { MiniPlayer } from '../../../components';
 //PAGES
 import Music from './Music';
 import Search from './Search';
-import Settings from './Settings';
 
 const Tab = createBottomTabNavigator();
 
@@ -24,14 +23,12 @@ function BottomTab() {
     
     const iconMusic = ({ color }) => <Icon name={"headset"} color={color} size={22} />;
     const iconSearch = ({ color }) => <Icon name={"search"} color={color} size={27} />;
-    const iconSettings = ({ color }) => <Icon name={"settings"} color={color} size={22} />;
     
     return (
         <Tab.Navigator backBehavior={'initialRoute'} tabBar={CustomTabBar} 
             tabBarOptions={{ inactiveTintColor: 'gray', tabStyle: { paddingBottom: 3 } }} >
             <Tab.Screen name={"Musicas"} component={Music} options={{ tabBarIcon: iconMusic }} />
             <Tab.Screen name={"Pesquisar"} component={Search} options={{ tabBarIcon: iconSearch }} />
-            <Tab.Screen name={"Configurações"} component={Settings} options={{ tabBarIcon: iconSettings }} />
         </Tab.Navigator>
     );
 
